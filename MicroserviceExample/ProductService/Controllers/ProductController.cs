f9dde605b6b5662338710bb9d6bb2afae4226d53
AnshuKumari904 <anshukumari904@gmail.com>
2023-11-29 18:55:08 +0530

    Adding Microservice Project
    Adding Microservice Project

---
+++ MicroserviceExample/ProductService/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Data;
+using ProductService.Modals;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _context.Products;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _context.Products.Where(x=>x.Id==id).FirstOrDefault();
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            try
+            {
+                _context.Products.Add(value);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, value);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
